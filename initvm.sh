1aebc1422b0e4fbb1b27624a2c432deb9ae82bfd
asdf57 <53836417+asdf57@users.noreply.github.com>
2024-06-18 13:38:40 -0400

    Refactor comments. Add additional mirrors.

--- initvm.sh
+++ initvm.sh
@@ -1,41 +1,33 @@
 #!/bin/bash
-# Boot into the Arch Linux live environment
+
+# This script is run inside the Arch Linux VM to initialize it
+# It is copied to the VM by the buildiso.sh script
 
 set -eux -o pipefail
 
-# Step 1: Partition, format, and mount the vda disk
-## Create the GPT partition table
+# Partition, format, and mount the vda disk
 sudo parted /dev/vda --script mklabel gpt
-## Create a single partition using the entire disk
 sudo parted /dev/vda --script mkpart primary ext4 1MiB 100%
-## Format the partition as ext4 filesystem
 sudo mkfs.ext4 /dev/vda1
-## Mount the partition to /mnt
 sudo mount /dev/vda1 /mnt
 
-# Step 2: Set up directories for overlay filesystem
-## Create upperdir directory for writable layer
-## Create workdir directory for work directory
+# Set up directories for overlay filesystem
 sudo mkdir -p /mnt/upperdir
 sudo mkdir -p /mnt/workdir
 
-# Step 4: Create overlay mount
-## Mount the overlay filesystem
-### -t: filesystem type
-### -o: mount options
-### lowerdir: read-only layer
-### upperdir: writable layer
-### workdir: work directory (used by overlayfs to store internal data)
+# Create overlay mount
 sudo mount -t overlay overlay -o lowerdir=/,upperdir=/mnt/upperdir,workdir=/mnt/workdir /mnt
 
+# Remove the existing resolv.conf file and copy the host's resolv.conf file to the new writable environment
 sudo rm /mnt/etc/resolv.conf
 sudo cp /etc/resolv.conf /mnt/etc/resolv.conf
 
-# Step 6: Chroot into the new writable environment
-## Bind mount /dev, /proc, and /sys to the new overlay filesystem
+# Mount the necessary filesystems for chroot
 sudo mount --bind /dev /mnt/dev
 sudo mount --bind /proc /mnt/proc
 sudo mount --bind /sys /mnt/sys
+
+# Chroot into the new environment
 sudo chroot /mnt
 
 # Initialize the pacman keyring
@@ -46,6 +38,12 @@ pacman-key --populate
 
 # Step 7: Populate mirrorlist
 cat <<EOF > /etc/pacman.d/mirrorlist
+Server = https://plug-mirror.rcac.purdue.edu/archlinux/\$repo/os/\$arch
+Server = https://mirror.csclub.uwaterloo.ca/archlinux/\$repo/os/\$arch
+Server = https://mirror.theash.xyz/arch/\$repo/os/\$arch
+Server = https://archlinux.uk.mirror.allworldit.com/archlinux/\$repo/os/\$arch
+Server = https://mirror.jordanrey.me/archlinux/\$repo/os/\$arch
+Server = https://mirror.pseudoform.org/\$repo/os/\$arch
 Server = http://mirrors.acm.wpi.edu/archlinux/\$repo/os/\$arch
 Server = http://mirror.adectra.com/archlinux/\$repo/os/\$arch
 Server = https://mirror.adectra.com/archlinux/\$repo/os/\$arch
