af9754240d181a9423fd7536197ed8cebcd4c426
asdf57 <53836417+asdf57@users.noreply.github.com>
2024-06-24 15:45:27 -0400

    Add additional packages to install. Disable SSH password auth in built ISO. Small script refactors

--- buildiso.sh
+++ buildiso.sh
@@ -21,15 +21,13 @@ fi
 
 USER="$(whoami)"
 LIMA_INSTANCE="arch-builder"
-SSH_KEY_PATH="$HOME/.ssh/arch_provisioning_key"
-ARCHLIVE_DIR="/home/$USER/archlive"
-SCRIPT_DIR="$(pwd)"
+HOST_SSH_KEY_PATH="$HOME/.ssh/arch_provisioning_key"
 VM_ARCHISO_DIR="/mnt/archiso"
 VM_SSH_KEY_PATH="/mnt/home/$USER/.ssh"
 
 # Generate a public key for Ansible SSH operations between host and target
-if [ ! -f "$SSH_KEY_PATH" ]; then
-  ssh-keygen -t ed25519 -f "$SSH_KEY_PATH" -N ""
+if [ ! -f "$HOST_SSH_KEY_PATH" ]; then
+  ssh-keygen -t ed25519 -f "$HOST_SSH_KEY_PATH" -N ""
 fi
 
 # Create a temporary Lima Arch VM to build the Arch image, removing any existing VM
@@ -45,7 +43,7 @@ limactl shell $LIMA_INSTANCE -- sudo mkdir -p ${VM_SSH_KEY_PATH}
 limactl shell $LIMA_INSTANCE -- sudo chown $USER:$USER ${VM_SSH_KEY_PATH}
 
 # Copy public keys to VM
-limactl copy "$SSH_KEY_PATH.pub" "${LIMA_INSTANCE}:${VM_SSH_KEY_PATH}"
+limactl copy "$HOST_SSH_KEY_PATH.pub" "${LIMA_INSTANCE}:${VM_SSH_KEY_PATH}"
 
 # Set proper permissions for the copied keys
 limactl shell $LIMA_INSTANCE -- chmod 644 /mnt/home/$USER/.ssh/arch_provisioning_key.pub
@@ -57,14 +55,17 @@ run_in_chroot pacman -S --noconfirm archiso openssh
 limactl shell $LIMA_INSTANCE -- sudo mkdir -p ${VM_ARCHISO_DIR}
 limactl shell $LIMA_INSTANCE -- sudo cp -r /mnt/usr/share/archiso/configs/releng/ ${VM_ARCHISO_DIR}
 
-# Add openssh to the packages list for the Arch ISO
-limactl shell $LIMA_INSTANCE -- sudo bash -c "echo -e 'openssh\njq' >> ${VM_ARCHISO_DIR}/releng/packages.x86_64"
+# Add required packages to packages list for the Arch ISO
+limactl shell $LIMA_INSTANCE -- sudo bash -c "echo -e 'openssh\njq\npython\npython-pip\nansible' >> ${VM_ARCHISO_DIR}/releng/packages.x86_64"
 
 # Add public key for SSH access
 run_in_chroot mkdir -p /archiso/releng/airootfs/root/.ssh/
 run_in_chroot bash -c "cat > /archiso/releng/airootfs/root/.ssh/authorized_keys" < /home/$USER/.ssh/arch_provisioning_key.pub
 
 run_in_chroot cat /archiso/releng/airootfs/root/.ssh/authorized_keys
+
+# Disable password authentication for SSH
+run_in_chroot sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /archiso/releng/airootfs/etc/ssh/sshd_config
 
 # Create the startup script
 run_in_chroot bash -c "cat > /archiso/releng/airootfs/root/startup.sh << 'EOF'
@@ -77,6 +78,10 @@ systemctl start sshd
 while ! ping -c 1 google.com &>/dev/null; do
   sleep 1
 done
+
+echo Available disks:
+
+lsblk
 
 # Print the IP address for the user
 echo 'Arch ISO is ready for Ansible provisioning. Use this IP to connect:'
