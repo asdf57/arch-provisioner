d5612c3ce278485bb70233a38252408035210f56
asdf57 <53836417+asdf57@users.noreply.github.com>
2024-08-18 14:29:34 -0400

    Add top level validators for all Partition attributes

--- server/schema.py
+++ server/schema.py
@@ -1,6 +1,45 @@
 from pydantic import BaseModel, field_validator
 from typing import List, Literal, Tuple, Union, Optional
 
+FLAGS = [
+    "bios_grub",
+    "legacy_boot",
+    "bls_boot",
+    "boot",
+    "msftdata",
+    "msftres",
+    "irst",
+    "esp",
+    "chromeos_kernel",
+    "lba",
+    "root",
+    "linux-home",
+    "swap",
+    "hidden",
+    "raid",
+    "LVM",
+    "PALO",
+    "PREP",
+    "DIAG"
+]
+
+FILE_SYSTEMS = [
+    "btrfs",
+    "ext2",
+    "ext3",
+    "ext4",
+    "fat16",
+    "fat32",
+    "hfs",
+    "hfs+",
+    "linux-swap",
+    "ntfs",
+    "reiserfs",
+    "udf",
+    "xfs"
+]
+
+
 def validate_size(value: str) -> str:
     print(value)
     if not value.endswith(('%', 'MiB', 'GiB')):
@@ -49,10 +88,84 @@ class Partition(BaseModel):
             raise ValueError(f"Invalid partition type: {v}")
         return v
 
+    @field_validator('align')
+    def validate_partition_align(cls, v):
+        if v not in ['cylinder', 'minimal', 'optimal', 'none', 'undefined']:
+            raise ValueError(f"Invalid alignment: {v}")
+        return v
+
+    @field_validator('flags')
+    def validate_partition_flags(cls, v):
+        if not all(flag in FLAGS for flag in v):
+            raise ValueError(f"Invalid flags: {v}")
+
+        return v
+
+    @field_validator('fs')
+    def validate_partition_fs(cls, v):
+        if v not in FILE_SYSTEMS:
+            raise ValueError(f"Invalid filesystem: {v}")
+        return v
+
+    @field_validator('label')
+    def validate_partition_label(cls, v):
+        max_length = 16
+
+        # Check if label length is within the limit
+        if len(v) > max_length:
+            raise ValueError(f"Label length must be less than or equal to {max_length} characters. Given: {v}")
+
+        # Check if all characters in the label are valid
+        if not all(char.isalnum() or char in '._-' for char in v):
+            raise ValueError(f"Label must only contain alphanumeric characters, '.', '_', or '-'. Given: {v}")
+
+        return v
+
+    @field_validator('name')
+    def validate_partition_name(cls, v):
+        max_length = 32
+
+        # Check if name length is within the limit
+        if len(v) > max_length:
+            raise ValueError(f"Name length must be less than or equal to {max_length} characters. Given: {v}")
+
+        # Check if all characters in the name are valid
+        if not all(char.isalnum() or char in ' ._-' for char in v):
+            raise ValueError(f"Name must only contain alphanumeric characters, '.', '_', or '-'. Given: {v}")
+
+        return v
+
+    @field_validator('number')
+    def validate_partition_number(cls, v):
+        if not v.isdigit():
+            raise ValueError(f"Partition number must be a positive integer. Given: {v}")
+
+        return v
+
     @field_validator('start', 'end')
-    @classmethod
     def validate_partition_size(cls, v):
         return validate_size(v)
+
+    @field_validator('resize')
+    def validate_partition_resize(cls, v):
+        if v not in ['true', 'false']:
+            raise ValueError(f"Resize must be either 'true' or 'false'. Given: {v}")
+
+        return v
+
+    @field_validator('state')
+    def validate_partition_state(cls, v):
+        if v not in ['present', 'absent', 'info']:
+            raise ValueError(f"State must be either 'present', 'absent', or 'info'. Given: {v}")
+
+        return v
+
+    @field_validator('unit')
+    def validate_partition_unit(cls, v):
+        if v not in ['MiB', 'GiB', '%']:
+            raise ValueError(f"Invalid unit: {v}")
+
+        return v
 
 class EFIPartition(Partition):
     """
