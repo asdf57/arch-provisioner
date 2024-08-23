b86b5cecdd8a1bf4b39fe444d544820af12a9c9b
asdf57 <53836417+asdf57@users.noreply.github.com>
2024-08-22 20:49:44 -0400

    Remove old provisioning script

--- provision.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ansible-playbook -i 'localhost,' \
-  -e 'ansible_host=localhost \
-      ansible_port=60022 \
-      ansible_user=root \
-      ansible_ssh_private_key_file=~/.ssh/arch_provisioning_key \
-      disk_device=/dev/vda' \
-  ansible/playbooks/pre_install.yml
