49482ce63d9c3ca3ac5f57856768e33f6e360440
asdf57 <53836417+asdf57@users.noreply.github.com>
2024-12-21 21:55:15 -0500

    Use new inventory file. Setup kube and python env vars. Copy all scripts to default dir

--- profile.d/setup.sh
+++ profile.d/setup.sh
@@ -14,10 +14,21 @@ ssh-add /home/condor/.ssh/git_provisioning_key >/dev/null 2>&1
 export PS1="[\u@\h]: "
 
 # Pull dotfiles from GitHub
-git clone git@github.com:asdf57/dotfiles.git /home/condor/dotfiles #>/dev/null 2>&1
+git clone git@github.com:asdf57/dotfiles.git /home/condor/dotfiles
 
 mkdir -p /home/condor/.kube
-ansible -b -i /home/condor/provision/ansible/inventory/inventory.yml -m fetch -a "src=/home/raspusr/.kube/config dest=/home/condor/.kube/config flat=yes" kube_coords[0]
+
+ansible -b -i /home/condor/provision/ansible/inventory/inventory.ini -m fetch -a "src=/home/root/.kube/config dest=/home/condor/.kube/config flat=yes" kube_coords[0]
+
+# Ensure kube config is not group or world readable
+chmod 600 /home/condor/.kube/config
+
+find /home/condor/provision/ansible/roles/ -type f -path '*/scripts/*' -exec cp {} /home/condor/provision \;
+
+
+export PYTHONPATH=/usr/local/lib/python3.12/site-packages:$PYTHONPATH
+export KUBECONFIG="/home/condor/.kube/config"
+
 echo "======================="
 echo "      hss v0.1.0"
 echo -e "=======================\n"
