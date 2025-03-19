33f2534886857b29c42555ebff6fb04337bf09ee
asdf57 <53836417+asdf57@users.noreply.github.com>
2025-03-19 19:41:16 -0400

    Use a different path for the nginx mount

--- ansible/roles/init/files/vault/entrypoint.sh
+++ ansible/roles/init/files/vault/entrypoint.sh
@@ -13,22 +13,22 @@ until curl -s http://127.0.0.1:8200/v1/sys/health | grep -q 'initialized'; do
 done
 
 # If keys have not been generated, initialize Vault
-if [ ! -f /vault/keys ]; then
+if [ ! -f /stuff/keys ]; then
   echo "Vault not initialized. Initializing now..."
-  vault operator init -key-shares=5 -key-threshold=3 -format=json > /vault/keys
-  jq -r '.root_token' /vault/keys > /vault/root_token
+  vault operator init -key-shares=5 -key-threshold=3 -format=json > /stuff/keys
+  jq -r '.root_token' /stuff/keys > /stuff/root_token
 else
   echo "Vault already initialized."
 fi
 
 echo "Unsealing Vault..."
-for key in $(jq -r '.unseal_keys_b64[]' /vault/keys | head -n 3); do
+for key in $(jq -r '.unseal_keys_b64[]' /stuff/keys | head -n 3); do
   vault operator unseal "$key"
 done
 
 echo "Vault is ready and operational."
 
-vault login "$(cat /vault/root_token)"
+vault login "$(cat /stuff/root_token)"
 
 if ! vault secrets list | grep -q "^kv2/"; then
   echo "Enabling KV version 2 secrets engine..."
