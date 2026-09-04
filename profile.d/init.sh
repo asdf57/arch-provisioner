f38ca53dac9c4781e2441f506fa0328c3364215f
asdf57 <53836417+asdf57@users.noreply.github.com>
2026-09-03 21:57:33 -0400

    Initial github ssh setup

--- profile.d/init.sh
+++ profile.d/init.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+
+mkdir -p /home/keiichi/.ssh
+
+mv $MOUNT_GIT_SSH_KEY_PATH /home/keiichi/.ssh/id_github
 
 git clone git@github.com:asdf57/inventory.git -b $INVENTORY_PUBLICATION_GROUP /home/keiichi
-
-
