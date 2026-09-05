d0c75e6c5ed0b73c77ef842b4d89425127e04238
asdf57 <53836417+asdf57@users.noreply.github.com>
2026-09-04 20:40:50 -0400

    General profile.d enhancements

--- profile.d/init.sh
+++ profile.d/init.sh
@@ -2,8 +2,8 @@
 
 ssh-keyscan github.com >> ~/.ssh/known_hosts
 
-mkdir -p /home/keiichi/.ssh
+mkdir -p /home/keiichi/.ssh /home/keiichi/inventory
 
-mv $MOUNT_GIT_SSH_KEY_PATH /home/keiichi/.ssh/id_github
+cp $MOUNT_GIT_SSH_KEY_PATH /home/keiichi/.ssh/id_github
 
-git clone git@github.com:asdf57/inventory.git -b $INVENTORY_PUBLICATION_GROUP /home/keiichi
+git clone git@github.com:asdf57/inventory.git -b $INVENTORY_PUBLICATION_GROUP /home/keiichi/inventory
